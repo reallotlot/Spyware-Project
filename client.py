316792e3e9c64a5bf0b5330e6b8bd7a807a04742
לוטן יהב <114772204+reallotlot@users.noreply.github.com>
2024-02-17 13:35:02 +0200

    fixed

--- client.py
+++ client.py
@@ -3,34 +3,16 @@
 
 #import outside libraries 
 import sys
-<<<<<<< HEAD
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 
 #import inside scripts
 from Spyware_Manager import detection
-=======
-from PyQt6.QtWidgets import *
-from PyQt6.QtGui import QIcon
->>>>>>> e632210294624364f29ffc6854071d33c3857c7c
 
-#import inside scripts
-from Spyware_Manager import detection
-
-<<<<<<< HEAD
 def show_res(output: QTextEdit):
     res = detection.ScanFile()
     text = "".join(str(i) for i in res if i != [])
-=======
-
-def show_res(output: QTextEdit):
-    res = detection.ScanFile()
-    text = ""
-    for i in res:
-        if i != []:
-            text = text.join(str(i))
->>>>>>> e632210294624364f29ffc6854071d33c3857c7c
     output.setText(text)
 
 def window():
@@ -45,7 +27,6 @@ def window():
     ico = QIcon("Design\\logo.png")
     win.setWindowIcon(ico)
     
-<<<<<<< HEAD
     button = QPushButton()
     button.setText("Scan")
     layout.addWidget(button)
@@ -59,20 +40,6 @@ def window():
     #buttons functions
     button.clicked.connect(lambda: show_res(output))
     
-=======
-    
-    button = QPushButton()
-    button.setText("Scan")
-    layout.addWidget(button)
-    
-    output = QTextEdit()
-    layout.addWidget(output)
-    
-    
-    #buttons functions
-    button.clicked.connect(lambda: show_res(output))
-    
->>>>>>> e632210294624364f29ffc6854071d33c3857c7c
     
     win.show()
     
